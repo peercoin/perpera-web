2126105989a06c881ea7cf168a4d5e40addb3897
Claudio <claudiohw@gmail.com>
2018-11-23 00:47:53 -0200

    Adding observable helper

---
+++ src/helpers/Observable.ts
@@ -0,0 +1,25 @@
+export default class ObservableHelper {
+  
+  public static on(eventName: string, cb: (payload?: any) => void) {
+    this.registerTrigger(eventName, cb);
+  }
+  
+  public static fire(eventName: string, payload?: any) {
+    const triggers = this.getTriggers(eventName);
+    triggers.forEach((cb: (payload: any) => void) => cb(payload));
+  }
+
+  private static triggers = {};
+
+  private static getTriggers(eventName: string) {
+    return this.triggers[eventName];
+  }
+
+  private static registerTrigger(eventName: string, cb: (payload?: any) => void) {
+    if (!this.triggers[eventName]) {
+      this.triggers[eventName] = [];
+    }
+
+    this.triggers[eventName].push(cb);
+  }
+}
