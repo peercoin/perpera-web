7c039a98a408519f4a5f8ddd3154cc498a38ca6c
Claudio <claudiohw@gmail.com>
2018-11-22 23:14:53 -0200

    Renaming some folders and adding header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import './Header.css';
+
+class Header extends React.Component {
+  public render() {
+    return (
+      <header className="HeaderComp">
+        <div className="container">
+          <h1 className="title">PERPERA</h1>
+          <h2 className="slogan">A Data Audit Protocol based on <a href="#">PeerAssets</a>.</h2>
+          <div className="by">
+            <span>by</span>
+            <img src="/img/peercoin-logo.svg" alt="Peercoin" width="95" />
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
