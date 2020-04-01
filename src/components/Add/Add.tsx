d09ed1ea86e55e523f8bf57a55d47f6c1d1c9469
bananenwilly <pussinbootsdev@gmail.com>
2020-04-01 13:37:29 +0200

    change icons, change responsive behaviour, eslint

--- src/components/Add/Add.tsx
+++ src/components/Add/Add.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import Loader from 'src/components/Loader/Loader';
-import ObservableHelper from 'src/helpers/Observable';
-import './Add.css';
+import * as React from "react";
+import Loader from "src/components/Loader/Loader";
+import ObservableHelper from "src/helpers/Observable";
+import "./Add.css";
 
 interface IState {
   errorMsg: string;
@@ -23,8 +23,8 @@ class Add extends React.Component<{}, IState> {
       errorMsg: "",
       fileType: true,
       isLoading: false,
-      uri: "",
-    }
+      uri: ""
+    };
   }
 
   public async handleFile(e: any) {
@@ -37,54 +37,54 @@ class Add extends React.Component<{}, IState> {
 
     const fileBuffer = await this.getFileBuffer(file);
     console.log(fileBuffer);
-    ObservableHelper.fire('onNewFileHash', {fileBuffer, fileName: file.name});
+    ObservableHelper.fire("onNewFileHash", { fileBuffer, fileName: file.name });
 
     this.setState({ isLoading: false });
   }
 
   public getFileBuffer(file: File): Promise<any> {
-    return new Promise((resolve) => {
+    return new Promise(resolve => {
       const reader = new FileReader();
-      
+
       reader.onloadend = () => {
         if (reader.readyState === reader.DONE) {
           resolve(reader.result);
         }
-      }
+      };
 
       reader.readAsArrayBuffer(file);
     });
   }
 
   public changeFileType(e: any) {
-    this.setState({fileType: !this.state.fileType, errorMsg: ""});
+    this.setState({ fileType: !this.state.fileType, errorMsg: "" });
   }
 
   public getFileSize(url: string) {
-      const http = new XMLHttpRequest();
-      http.open('HEAD', url, false); // false = Synchronous
-  
-      http.send(null); // it will stop here until this http request is complete
-  
-      // when we are here, we already have a response, b/c we used Synchronous XHR
-  
-      if (http.status === 200) {
-          const fileSize = http.getResponseHeader('content-length');
-          if (fileSize != null) {
-            return parseInt(fileSize, 10);
-          }
+    const http = new XMLHttpRequest();
+    http.open("HEAD", url, false); // false = Synchronous
+
+    http.send(null); // it will stop here until this http request is complete
+
+    // when we are here, we already have a response, b/c we used Synchronous XHR
+
+    if (http.status === 200) {
+      const fileSize = http.getResponseHeader("content-length");
+      if (fileSize != null) {
+        return parseInt(fileSize, 10);
       }
-      return -1;
+    }
+    return -1;
   }
 
   public async getUriContent(uri: string): Promise<any> {
     const controller = new AbortController();
     const signal = controller.signal;
 
-    const timerID = setTimeout( (() => {
+    const timerID = setTimeout(
+      (() => {
         controller.abort();
-      })
-      .bind(this),
+      }).bind(this),
       10 * 60 * 1000
     );
 
@@ -95,15 +95,15 @@ class Add extends React.Component<{}, IState> {
       return 1;
     }
 
-    const response = await fetch(uri, {method: 'get', signal});
+    const response = await fetch(uri, { method: "get", signal });
     console.log(response);
-    clearTimeout(timerID)
+    clearTimeout(timerID);
     if (!response.ok) {
       return 0;
     }
     return response.blob().toString();
   }
-  
+
   public async handleFileUri(e: any) {
     e.preventDefault();
     this.setState({ isLoading: true });
@@ -111,17 +111,18 @@ class Add extends React.Component<{}, IState> {
     try {
       const fileBuffer = await this.getUriContent(this.state.uri);
       if (fileBuffer === 0) {
-        this.setState({ errorMsg: "document URI invalid"});  
-      }
-      else if (fileBuffer === 1) {
-        this.setState({ errorMsg: "document URI invalid"});  
-      }
-      else {
-        ObservableHelper.fire('onNewFileHash', {fileBuffer, fileName: this.state.uri});
-        this.setState({fileType: !this.state.fileType});
+        this.setState({ errorMsg: "document URI invalid" });
+      } else if (fileBuffer === 1) {
+        this.setState({ errorMsg: "document URI invalid" });
+      } else {
+        ObservableHelper.fire("onNewFileHash", {
+          fileBuffer,
+          fileName: this.state.uri
+        });
+        this.setState({ fileType: !this.state.fileType });
       }
     } catch (e) {
-      this.setState({ errorMsg: "error on adding URI"});
+      this.setState({ errorMsg: "error on adding URI" });
     }
 
     this.setState({ isLoading: false });
@@ -135,8 +136,22 @@ class Add extends React.Component<{}, IState> {
     return (
       <div className="AddComp">
         {this.state.isLoading && <Loader />}
-        <div className="add-btn" title="input file">+<input className="file" type="file" onChange={this.handleFile}/></div>
-        <div className="add-btn add-uri" title="input uri">+<input className="file" type="button" onClick={this.changeFileType}/></div>
+        <div className="add-btn add-file" title="input file">
+          <img
+            className="add-icon"
+            src="/img/attach-file.svg"
+            alt="attach-file"
+          />
+          <input className="file" type="file" onChange={this.handleFile} />
+        </div>
+        <div className="add-btn add-uri" title="input uri">
+          <img
+            className="add-icon"
+            src="/img/attach-uri.svg"
+            alt="attach uri"
+          />
+          <input className="file" type="button" onClick={this.changeFileType} />
+        </div>
         {!this.state.fileType && (
           <div className="input-uri">
             <div className="input-dlg">
@@ -148,21 +163,18 @@ class Add extends React.Component<{}, IState> {
                   <label>Insert document URI:</label>
                 </div>
                 <div>
-                  <input 
-                  className="form-field" 
-                  placeholder="Type URI here..."
-                  type="url" 
-                  pattern="https?://.+" 
-                  onChange={this.handleURI}/>
+                  <input
+                    className="form-field"
+                    placeholder="Type URI here..."
+                    type="url"
+                    pattern="https?://.+"
+                    onChange={this.handleURI}
+                  />
                 </div>
                 {this.state.errorMsg && (
                   <div className="error-msg">{this.state.errorMsg}</div>
                 )}
-                <button
-                  className="form-submit"
-                >
-                  Add Uri
-                </button>
+                <button className="form-submit">Add Uri</button>
               </form>
             </div>
           </div>
--- src/components/Status/Status.tsx
+++ src/components/Status/Status.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react';
-import Loader from 'src/components/Loader/Loader';
-import './Status.css';
+import * as React from "react";
+import Loader from "src/components/Loader/Loader";
+import "./Status.css";
 
 interface IState {
-    apiStatus: boolean;
-    isLoading: boolean;
+  apiStatus: boolean;
+  isLoading: boolean;
 }
 
 class Status extends React.Component<{}, IState> {
@@ -13,32 +13,40 @@ class Status extends React.Component<{}, IState> {
     this.state = {
       apiStatus: false,
       isLoading: false
-    }
+    };
   }
-  public componentDidMount(){
+  public componentDidMount() {
     // simplest solution : used getaddress API with old address
     fetch("https://blockbook.peercoin.net/api")
-       .then()
-       .then(
-         (result) => {
-           this.setState({
-             apiStatus: true
-           });
-         },
-         (error) => {
-           this.setState({
-             apiStatus: false
-           });
-         }
-       )
+      .then()
+      .then(
+        result => {
+          this.setState({
+            apiStatus: true
+          });
+        },
+        error => {
+          this.setState({
+            apiStatus: false
+          });
+        }
+      );
   }
 
   public render() {
     return (
       <div className="StatusComp">
         {this.state.isLoading && <Loader />}
-        {this.state.apiStatus && <div className="status-btn"><img src="img/status_on.svg"/></div>}
-        {!this.state.apiStatus && <div className="status-btn"><img src="img/status_off.svg"/></div>}
+        {this.state.apiStatus && (
+          <div className="status-btn">
+            <img src="img/status_on.svg" alt="connected" />
+          </div>
+        )}
+        {!this.state.apiStatus && (
+          <div className="status-btn">
+            <img src="img/status_off.svg/" alt="disconnected" />
+          </div>
+        )}
       </div>
     );
   }
